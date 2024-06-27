42bfd1c1dccc310d8b8ea3be62e1f19b612b04ef
Efran Pasaribu <'efranr@xl.co.id'>
2024-06-27 21:32:35 +0700

    update

---
+++ Object Repository/Page_Log In  WordPress.com/div_login_wordpress.rs
@@ -0,0 +1,119 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_login_wordpress</name>
+   <tag></tag>
+   <elementGuidId>f0efd297-d07e-40d1-ad33-4afb2750b621</elementGuidId>
+   <imagePath>Screenshots/Targets/Page_Log In  WordPress.com/div_Log in to WordPress.com.png</imagePath>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.login__form-header</value>
+      </entry>
+      <entry>
+         <key>IMAGE</key>
+         <value>Screenshots/Targets/Page_Log In  WordPress.com/div_Log in to WordPress.com.png</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='primary']/div/main/div[2]/div/div/div[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:text=&quot;Log in to WordPress.com&quot;i</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>210c3f4b-b4b8-42de-8f47-fc248c80ca89</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>login__form-header</value>
+      <webElementGuid>9fb8cbe9-8104-45d6-a93f-7b5b9729b920</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Log in to WordPress.com</value>
+      <webElementGuid>9bfaf570-29a4-4a22-bd5e-d308d055e00b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;primary&quot;)/div[1]/main[@class=&quot;wp-login__main is-social-first main&quot;]/div[@class=&quot;wp-login__container&quot;]/div[@class=&quot;login&quot;]/div[@class=&quot;login__form-header-wrapper&quot;]/div[@class=&quot;login__form-header&quot;]</value>
+      <webElementGuid>a5df36e6-274e-4ee8-9c1a-291c669b8f0a</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='primary']/div/main/div[2]/div/div/div[2]</value>
+      <webElementGuid>806d4844-6fd4-4455-bd08-40ad7968ef36</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Create an account'])[1]/following::div[5]</value>
+      <webElementGuid>6c4602c3-25ee-4db8-9943-d557d071caa5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Terms of Service'])[1]/preceding::div[1]</value>
+      <webElementGuid>2fc850c0-7a56-480c-b5a7-d5bb679e133e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Privacy Policy'])[1]/preceding::div[1]</value>
+      <webElementGuid>e5f0be3d-4125-49b5-8a37-361e1b485659</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Log in to WordPress.com']/parent::*</value>
+      <webElementGuid>fd10246c-63f9-488e-81eb-cb56e3f1bfdd</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div/div[2]</value>
+      <webElementGuid>eee99b58-98d4-4ecd-998b-a1866368cf34</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Log in to WordPress.com' or . = 'Log in to WordPress.com')]</value>
+      <webElementGuid>af1212a6-97da-4cb6-9a59-f4d7baade6c6</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/TC_Text_Log_in_wordpress.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TC_Text_Log_in_wordpress</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>930c5e2e-0bb5-4c2c-a0c7-a2c5111bdf3d</testSuiteGuid>
+   <testCaseLink>
+      <guid>e21a731a-fe37-432a-93a7-f40aed0082a4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Test Cases/TC_Text_Log_in_wordpress</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
